2039356c86350b529fbcbc2413425927761ff7f2
Andika Putra <andikaputra@Andikas-MacBook-Air.local>
2015-10-06 21:59:42 -0400

    Add share text functionality.

--- app/src/main/java/com/example/smartsign/smartsignservice/MainActivity.java
+++ app/src/main/java/com/example/smartsign/smartsignservice/MainActivity.java
@@ -1,16 +1,41 @@
 package com.example.smartsign.smartsignservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent here...
+            } else {
+                setContentView(R.layout.activity_main); // Handle invalid sent data type here...
+            }
+        } else {
+            setContentView(R.layout.activity_main); // Go to the settings page.
+        }
+    }
+
+    // Handle text sent from 'share' in other app.
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            Toast toast = Toast.makeText(getApplicationContext(), sharedText, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     @Override
